4a3c79b31df10fe1fa60222056f80a00318a6798
Manan Kandpal <manank.7409@gmail.com>
2024-07-14 13:28:12 +0530

    Starting 30 day JavaScript Challenge

---
+++ day1-variables.js
@@ -0,0 +1,45 @@
+// Activity 1
+var a = 5
+console.log(a)
+let s = "Manan"
+console.log(s)
+
+// Activity 2
+const b = true
+console.log(b)
+
+// Activity 3
+let num = 5
+console.log(typeof(num))
+let str = "Manan Kandpal"
+console.log(typeof(str))
+let bool = false
+console.log(typeof(bool))
+let student ={
+    name: "Manan",
+    age: 21,
+    role: "Coder"
+}
+console.log(typeof(student))
+let arr = [1,2,3,4,5,6]
+console.log(typeof(arr))
+
+// Activity 4
+const challenge = "30 days JS Challenge!"
+console.log(challenge)
+
+// challenge = "JS Challenge"
+// console.log(challenge)
+
+// Observing error
+// challenge = "JS Challenge"
+//           ^
+
+// TypeError: Assignment to constant variable.
+//     at Object.<anonymous> (D:\manan\JavaScript\js-hindi-youtube-main\30 days JS Challenge\day1.js:30:11)
+//     at Module._compile (node:internal/modules/cjs/loader:1241:14)
+//     at Module._extensions..js (node:internal/modules/cjs/loader:1295:10)
+//     at Module.load (node:internal/modules/cjs/loader:1091:32)
+//     at Module._load (node:internal/modules/cjs/loader:938:12)
+//     at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:83:12)
+//     at node:internal/main/run_main_module:23:47
---
+++ day2-operators.js
@@ -0,0 +1,42 @@
+// Activity 1
+
+//task1
+let a = 5
+let b = 4
+console.log("Addition =",a+b)
+
+//task 2
+let num1 = 8
+let num2 = 5
+console.log("Subtraction =",num1-num2)
+
+//task 3
+let n1 =  25
+let n2 = 25
+console.log("Multiplication =", n1*n2)
+
+//task4
+let number1= 20
+let number2= 4
+console.log("Division =", number1/number2)
+
+//task5
+let c = 54
+let d = 25
+console.log("Reminder =", c%d)
+
+//Activity 2
+//task6
+let originalNum1 = 10
+console.log("Original Number 1:", originalNum1)
+originalNum1+= 5
+console.log("Number 1 after adding 5 to it:", originalNum1)
+
+//task7
+let originalNum2 = 10
+console.log("Original Number 2:", originalNum2)
+originalNum2-= 5
+console.log("Number 2 after subtracting 5 from it:", originalNum2)
+
+//Activity 3
+//task8
