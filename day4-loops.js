aa60a6b2fc80e7df109520c10edf459c85c1be6c
Manan Kandpal <manank.7409@gmail.com>
2024-07-17 01:09:12 +0530

    Day4 challenge started

---
+++ day4-loops.js
@@ -0,0 +1,32 @@
+//Activity 1
+//Task1
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+//Task2
+for(let i=1; i<=10; i++){
+    console.log("5 x",i, "=", 5*i);
+}
+//Activity 2
+//Task3
+let n=1
+let sum=0
+while(n<=10){
+    sum+=n;
+    n++;
+}
+console.log("Sum of numbers 1 to 10 =",sum)
+//Task4
+let num = 10
+while(num!=0){
+    console.log(num)
+    num--;
+}
+//Activity 3
+//Task5
+//Task6
+//Activity 4
+//Task7
+//Activity 5
+//Task8
+//Task9
