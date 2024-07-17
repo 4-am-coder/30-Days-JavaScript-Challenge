a2366264dccf0595e813f2c324007079f135f42c
Manan Kandpal <manank.7409@gmail.com>
2024-07-17 08:51:11 +0530

    Day4 Challenge Completed

--- day4-loops.js
+++ day4-loops.js
@@ -24,9 +24,52 @@ while(num!=0){
 }
 //Activity 3
 //Task5
+console.log("Task 5:")
+let j=1
+do{
+    console.log(j)
+    j++
+} 
+while(j<=5)
 //Task6
+let number = 5
+let fact =1;
+if(number <0){
+    console.log("Please provide a positive number for finding factorial")
+} else{
+    if(number!=0){
+        do{
+            fact*=number
+            number--;
+        } while(number!=0)
+    }
+    console.log("factorial:", fact)
+}
+
 //Activity 4
 //Task7
+console.log("Pattern:")
+let result = ""
+for(let i=1; i<=5; i++){
+    for(let j=1; j<=i; j++){
+        result+="* "
+    }
+    result+="\n"
+}
+console.log(result)
 //Activity 5
 //Task8
+for(let i=1; i<=10; i++){
+    if(i===5){
+        continue;
+    } else {
+        console.log(i);
+    }
+}
 //Task9
+for(let i=1; i<=10; i++){
+    if(i===7){
+        break;
+    }
+    console.log(i);
+}
